824f7f690766f49382916a5bb150d4c31f9c2d84
李扬 <ly18842607263@gmail.com>
2016-03-13 16:01:22 +0800

    Initial Commit

---
+++ livetv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  livetv
+//
+//  Created by 李扬 on 16/3/13.
+//  Copyright © 2016年 李扬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
